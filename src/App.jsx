1f35c38a87f8cbab7e971ddd81d8c989bbcc9d63
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-01-31 16:50:07 -0500

    updeted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Home from "./components/Home";
-import {Routes, Route} from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
+      <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      
+      </div>
     </div>
   );
 }
