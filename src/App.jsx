a3e26c37ca95867eaaa43cd0e3670e8d8000f44a
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-01-31 19:31:46 -0500

    js

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Donate from "./components/Donate";
 import "./carousel.css"
-import Donate from "./components/Donate";
 
 function App() {
   return (
